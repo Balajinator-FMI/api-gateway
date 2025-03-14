5a6f4c80cef6ed7562ac9725e0d3420ea988ce39
Aleko Georgiev <algugone@gmail.com>
2025-03-14 18:45:39 +0200

    API Gateway Initial Commig

---
+++ src/main/java/com/fmicodesproj/fmicodesprojgateway/FmiCodesProjGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.fmicodesproj.fmicodesprojgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmiCodesProjGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FmiCodesProjGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fmicodesproj/fmicodesprojgateway/FmiCodesProjGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmicodesproj.fmicodesprojgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmiCodesProjGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
